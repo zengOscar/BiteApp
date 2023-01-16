fe2053a03de6dbcba24e1b1b45dbed30ac3a0faf
OSCAR ZENG <ozeng@expediagroup.com>
2023-01-16 13:40:15 -0600

    Setting up app structure

---
+++ Screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class SearchScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>This is the search screen.</Text>
+      </View>
+    );
+  }
+}
+
+export default SearchScreen;
